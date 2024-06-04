f7c1570da0236262381927aac1f53a64e276d690
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2024-06-04 14:04:10 -0700

    tested save and delete methods in audiofile

--- src/code/Driver.java
+++ src/code/Driver.java
@@ -13,15 +13,22 @@ public class Driver
         final MusicMedia vinylRecord;
 
         cd = new CompactDisc("In the Shadows", "The Rasmus", 1);
-        audioFile = new AudioFile("Billie Jean", "Michael Jackson", "billie.mp3", 1250);
+        audioFile = new AudioFile("Billie Jean", "Michael Jackson", "billie.mp3", 12500);
         vinylRecord = new VinylRecord("Si pudiera", "Manuel Medrano", 1, 7, 40);
 
         System.out.println(cd);
         System.out.println(audioFile);
         System.out.println(vinylRecord);
 
+        System.out.println();
+
         cd.play();
         audioFile.play();
         vinylRecord.play();
+
+        System.out.println();
+
+        ((AudioFile) audioFile).save("In the Shadows");
+        ((AudioFile) audioFile).delete("In the Shadows");
     }
 }
